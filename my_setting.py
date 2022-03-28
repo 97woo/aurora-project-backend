1f75958f0ed308be93da418e8c411c6d3bdc581f
park geon woo <parkgeonwoo@parks-MacBook-Air.local>
2022-03-28 10:56:01 +0900

    modify: my_settings

--- my_setting.py
+++ my_setting.py
@@ -10,14 +10,7 @@ DATABASES = {
     }
 }
         
-SECRET_KEY = 'django-insecure-w&ar0+tl8rk4a=&63i6=z*&x!xjj@3!!i=fq%vv*%&gsoo2@+-'
+SECRET_KEY = 'django-insecure-6lj@1#fyx4(+ylaxfuk1@o8ea76c7$5n4y3-h=s@@oobae=+r7'
 
 ALGORITHM ="HS256"
 
-AWS_ACCESS_KEY_ID='AKIAVVTYAG3O3VNMEG5Q'
-AWS_SECRET_ACCESS_KEY='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'
-AWS_STORAGE_BUCKET_NAME='woosbucket'
-BUCKET_ADDRESS    = 'https://woosbucket.s3.ap-northeast-2.amazonaws.com'
-BUCKET_DIR_THUMBNAIL  = 'thumbnail'
-BUCKET_DIR_IMAGE = 'image'
-BUCKET_DIR_PROFILE = 'profile'
